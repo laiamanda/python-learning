8d8d3c7e8be81e1e5f90e6ceec2ab616154f8dac
laiamanda <40805270+laiamanda@users.noreply.github.com>
2023-04-12 20:38:01 -0400

    Add /packages_tutorial

---
+++ packages_tutorial/myprogram.py
@@ -0,0 +1,12 @@
+""" 
+from mymodule import my_func
+
+my_func()
+"""
+
+from myMainPackage import some_main_script
+from myMainPackage.subPackage import some_sub_script
+
+some_main_script.report_main()
+
+some_sub_script.sub_report()
